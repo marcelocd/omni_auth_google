bdb22a6d3cf33826a19208e0bbb3f54f75218d36
Marcelo Dias <marcelocdias93@gmail.com>
2025-02-11 12:22:10 -0300

    Implement Google OAuth2 authentication for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.full_name = auth.info.name
+      user.avatar_url = auth.info.image
+      # if you are using confirmable and confirmation emails,
+      # uncomment the line below to skip the confirmation emails
+      # user.skip_confirmation!
+    end
+  end
 end
